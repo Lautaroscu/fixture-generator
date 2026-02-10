9652aecb7f68a9151fc52ac4ec797ba2323b040c
Lautaroscu <scuffilautaro@gmail.com>
2026-02-09 21:54:50 -0300

    agrega netlify domain to cors

--- src/main/java/com/fixture/fixturesservice/config/CorsGlobalConfiguration.java
+++ src/main/java/com/fixture/fixturesservice/config/CorsGlobalConfiguration.java
@@ -14,7 +14,7 @@ public class CorsGlobalConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000" , "https://ligatandil.netlify.app")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
