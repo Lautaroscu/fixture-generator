16400e15408e0e40eab659f95c353b3a38968070
Lautaroscu <scuffilautaro@gmail.com>
2026-02-20 17:14:56 -0300

    enable async

--- src/main/java/com/fixture/fixturesservice/FixturesServiceApplication.java
+++ src/main/java/com/fixture/fixturesservice/FixturesServiceApplication.java
@@ -1,7 +1,5 @@
 package com.fixture.fixturesservice;
 
-import com.fixture.fixturesservice.services.FixtureService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
