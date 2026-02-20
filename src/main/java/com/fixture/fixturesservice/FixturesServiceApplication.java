482f19fd2e141decd14a69b632958c5764c01c35
Lautaroscu <scuffilautaro@gmail.com>
2026-02-20 17:19:17 -0300

    enable async

--- src/main/java/com/fixture/fixturesservice/FixturesServiceApplication.java
+++ src/main/java/com/fixture/fixturesservice/FixturesServiceApplication.java
@@ -2,8 +2,10 @@ package com.fixture.fixturesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class FixturesServiceApplication {
     public static void main(String[] args) {
 
